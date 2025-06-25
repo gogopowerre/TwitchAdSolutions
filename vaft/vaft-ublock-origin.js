296e24cc64162cdbc56f03ff87fe445b4cea3421
gogopowerre <freegtavsss@gmail.com>
2025-06-25 10:56:40 +0300

    Update vaft-ublock-origin.js

--- vaft/vaft-ublock-origin.js
+++ vaft/vaft-ublock-origin.js
@@ -14,8 +14,8 @@ twitch-videoad.js text/javascript
         scope.ClientVersion = 'null';
         scope.ClientSession = 'null';
         scope.PlayerType2 = 'embed'; //Source
-        scope.PlayerType3 = 'site'; //Source
-        scope.PlayerType4 = 'autoplay'; //360p
+        scope.PlayerType3 = 'embed'; //Source
+        scope.PlayerType4 = 'embed'; //360p
         scope.CurrentChannelName = null;
         scope.UsherParams = null;
         scope.WasShowingAd = false;
